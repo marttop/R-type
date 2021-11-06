d2b48ff69a7f0ba9397204d46d0f2aea374564a4
Marton Szuts <marton.szuts@epitech.eu>
2021-11-06 14:41:20 +0100

    Added new main for client testing Cmake generating new binary "rtype_testclient"

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -5,81 +5,15 @@
 ** main
 */
 
-#include <iostream>
-#include <boost/array.hpp>
-#include <boost/asio.hpp>
-       #include <sys/types.h>
-       #include <unistd.h>
+#include "Window.hpp"
 
-using boost::asio::ip::tcp;
+typedef  int (Window::*func)(void);
 
-int main(int argc, char* argv[])
+int main(void)
 {
-    try
-    {
-        if (argc != 2)
-        {
-            std::cerr << "Usage: client <host>" << std::endl;
-            return 1;
-        }
+    Window window("R-TYPE");
 
-        boost::asio::io_context io_context;
-        tcp::resolver resolver(io_context);
-        std::cout << argv[1] << std::endl;
-        auto endpoints = tcp::endpoint( boost::asio::ip::address::from_string(argv[1]), 8888);
-        tcp::socket socket(io_context);
-            boost::array<char, 128> buf;
-            boost::system::error_code error;
-        pid_t eric;
+    window.gameLoop();
 
-        socket.connect( endpoints);
-            eric = fork();
-        for (;;)
-        {
-
-
-
-            if (eric == 0) {
-
-                size_t len = socket.read_some(boost::asio::buffer(buf), error);
-                std::cout.write(buf.data(), len);
-            }
-            else {
-
-                if (error == boost::asio::error::eof)
-                    break; // Connection closed cleanly by peer.
-                else if (error)
-                    throw boost::system::system_error(error); // Some other error.
-
-                std::string test = "";
-
-                std::cout << "msg: ";
-                std::cin >> test;
-
-                test += "\r\n";
-
-                socket.write_some(boost::asio::buffer(test), error);
-
-            }
-        }
-    }
-    catch (std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-
-    return 0;
+    return EXIT_SUCCESS;
 }
-
-// #include "Window.hpp"
-
-// typedef  int (Window::*func)(void);
-
-// int main(void)
-// {
-//     Window window("R-TYPE");
-
-//     window.gameLoop();
-
-//     return EXIT_SUCCESS;
-// }
