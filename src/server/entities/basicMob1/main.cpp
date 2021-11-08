6e47f01d4a84ae392113841b93c75d52495fa972
antonin <antonin.fille@epitech.eu>
2021-11-08 21:55:15 +0100

    singeltone entity loader for mob

--- src/server/entities/basicMob1/main.cpp
+++ src/server/entities/basicMob1/main.cpp
@@ -1,24 +1,33 @@
 
 
-#include "IEntity.hpp"
-#include <dlfcn.h>
+#include <EntityLoad.hpp>
 
 int main()
 {
   std::cout << "loader" << std::endl;
 
   /* on Linux, use "./myclass.so" */
-  void* handle = dlopen("./basicMob1.so", RTLD_LOCAL | RTLD_LAZY);
+  // void* handle = dlopen("./basicMob1.so", RTLD_LOCAL | RTLD_LAZY);
 
-  IEntity* (*create)();
-  void (*destroy)(IEntity*);
 
-  create = (IEntity* (*)())dlsym(handle, "allocator");
-  destroy = (void (*)(IEntity*))dlsym(handle, "deleter");
+  // IEntity* (*create)();
+  // void (*destroy)(IEntity*);
 
-  IEntity* myClass = (IEntity*)create();
-  myClass->update();
-  destroy( myClass );
+  // create = (IEntity* (*)())dlsym(handle, "allocator");
+  // destroy = (void (*)(IEntity*))dlsym(handle, "deleter");
+
+  // IEntity* myClass = (IEntity*)create();
+  // myClass->update();
+  // destroy( myClass );
+
+  IEntity *mob1;
+
+  EntityLoad &loader = EntityLoad::getEntityLoader();
+
+  mob1 = loader.loadEntityWithPath("./basicMob1.so");
+
+  mob1->update();
+
 
   return 0;
 }
