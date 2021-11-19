c24ff8c793c22737e73c46fdbcb8654196b65e72
Lorentz-Andrea Romeo <lorentz-andrea.romeo@epitech.eu>
2021-11-19 17:55:12 +0100

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ echo "Removing build folder..."
 rm -rf build
 
 echo "Building shared libraries..."
-cd src/server/entities && make
+cd src/server/entities && make && cd ../../..
 
 echo "Building project..."
 mkdir build && cd build && conan install -u .. --build=missing && cmake .. -G "Unix Makefiles" && cmake --build .
