5b7c4627aac0445d17397bd90a9b25a0f2829bfd
Lorentz-Andrea Romeo <lorentz-andrea.romeo@epitech.eu>
2021-11-21 20:19:50 +0100

    Sprite fix and particles

--- src/client/entity/BoomrangBulletC.cpp
+++ src/client/entity/BoomrangBulletC.cpp
@@ -6,13 +6,17 @@
 */
 
 #include "BoomrangBulletC.hpp"
-#include <iostream>
 
 BoomrangBulletC::BoomrangBulletC(const sf::Texture &texture, const sf::Vector2f &pos, const float &speed, const sf::Color &startColor, const sf::Color &endColor, int health)
     : Entity(texture, pos, speed, startColor, endColor, health)
 {
     _sprite.setTextureRect(sf::IntRect(sf::Vector2i(_sprite.getLocalBounds().width / 12 * (std::rand() % 11 + 1), 0), sf::Vector2i(_sprite.getLocalBounds().width / 12, _sprite.getLocalBounds().height)));
     _sprite.setScale(sf::Vector2f(4, 4));
+
+    _shootBuf = AssetManager<sf::SoundBuffer>::getAssetManager().getAsset("assets/sounds/boss_shoot.ogg");
+    _shoot.setBuffer(_shootBuf);
+    _shoot.setVolume(20);
+    _shoot.play();
 }
 
 BoomrangBulletC::~BoomrangBulletC()
@@ -24,7 +28,7 @@ void BoomrangBulletC::update()
     if (!_isAlive)
         _deathFinish = true;
 
-    _particleSystem.update(sf::Vector2f{25, 25}, sf::Vector2f{_pos.x + _sprite.getGlobalBounds().width / 2, _pos.y + _sprite.getGlobalBounds().height / 2}, sf::Vector2f{_pos.x + _sprite.getGlobalBounds().width / 2, _pos.y + _sprite.getGlobalBounds().height / 2}, sf::Color(0, 255, 255, 200), sf::Color(255, 255, 255, 50), 20, 1);
+    _particleSystem.update(sf::Vector2f{40, 40}, sf::Vector2f{_pos.x + _sprite.getGlobalBounds().width / 2, _pos.y + _sprite.getGlobalBounds().height / 2}, sf::Vector2f{_pos.x + _sprite.getGlobalBounds().width / 2, _pos.y + _sprite.getGlobalBounds().height / 2}, sf::Color(0, 255, 255, 200), sf::Color(255, 255, 255, 50), 10, 1);
 
     if (_animationClock.getElapsedTime().asMilliseconds() > 50) {
         _animationClock.restart();
--- src/client/entity/BoomrangBulletC.hpp
+++ src/client/entity/BoomrangBulletC.hpp
@@ -9,23 +9,26 @@
 #define BOOMRANGBULLETC_HPP__
 
 #include "Entity.hpp"
+#include "AssetManager.hpp"
 
 class BoomrangBulletC : public Entity {
-public:
-    BoomrangBulletC(const sf::Texture &texture,
-                const sf::Vector2f &pos,
-                const float &speed,
-                const sf::Color &startColor,
-                const sf::Color &endColor,
-                int health);
-    ~BoomrangBulletC();
+    public:
+        BoomrangBulletC(const sf::Texture &texture,
+                    const sf::Vector2f &pos,
+                    const float &speed,
+                    const sf::Color &startColor,
+                    const sf::Color &endColor,
+                    int health);
+        ~BoomrangBulletC();
 
-    void update() override;
-    void drawSprite(sf::RenderWindow &window) override;
-    void drawParticles(sf::RenderWindow &window) override;
+        void update() override;
+        void drawSprite(sf::RenderWindow &window) override;
+        void drawParticles(sf::RenderWindow &window) override;
 
-protected:
-private:
+    protected:
+    private:
+        sf::SoundBuffer _shootBuf;
+        sf::Sound _shoot;
 };
 
 #endif /* !BOOMRANGBULLETC_HPP__ */
